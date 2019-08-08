a51ed527ec0c9ef29c78cf34bf7053cdccf82284
yonybrondy <yonybrondy17@gmail.com>
2019-08-08 11:51:41 -0500

    modificacion en vista de formato

--- application/controllers/backend/Aportes.php
+++ application/controllers/backend/Aportes.php
@@ -5,9 +5,9 @@ class Aportes extends CI_Controller {
 
 	public function __construct(){
 		parent::__construct();
-		if (!$this->session->userdata("login")) {
-			redirect(base_url());
-		}
+		// if (!$this->session->userdata("login")) {
+		// 	redirect(base_url());
+		// }
 
 		$this->load->model("Aportes_model");
 		$this->load->library('excel');
--- application/views/admin/aportes/formato.php
+++ application/views/admin/aportes/formato.php
@@ -6,6 +6,19 @@
 	<style>
 		html, body{
 			font-size: 10px;
+			margin: 0;
+			padding: 20px;
+		}
+		table{
+			border: 1px solid #000;
+			margin: 0;
+
+		}
+		.titulo{
+			border: 1px solid #000;
+		}
+		table tr td , table tr th{
+			border: 0px;
 		}
 		.texto-centrado{
 			text-align: center;
@@ -16,10 +29,10 @@
 	</style>
 </head>
 <body>
-	<p class="texto-centrado">FONDO DE EMPLEADOS DE <br>
+	<p class="texto-centrado"> <strong>FONDO DE EMPLEADOS DE <br>
 	SUNSHINE BOUQUET<br>
-	NIT. 830.039.546-9</p>
-	<table style="width: 100%;" cellpadding="3" cellspacing="0" border="1">
+	NIT. 830.039.546-9 </strong></p>
+	<table style="width: 100%;" cellpadding="3" cellspacing="0">
 		<tr>
 			<td>NOMBRE</td>
 			<td colspan="2">CACERES MARTINEZ GISEL</td>
@@ -53,14 +66,229 @@
 		<tr>
 			<td colspan="2">FECHA AFILIACION</td>
 			<td>2001.11.29</td>
-			<td>CUOTA OBLIGATORIA</td>
+			<th>CUOTA OBLIGATORIA</th>
 			<td class="texto-derecho">58,905</td>
 		</tr>
 		<tr>
 			<td colspan="2">FECHA DE INGRESO ALA EMPRESA</td>
 			<td>2000.05.13</td>
-			<td>APO+AHO EN MORA</td>
+			<th>APO+AHO EN MORA</th>
 			<td class="texto-derecho">0</td>
+		</tr>
+		<tr>
+			<th colspan="5" style="font-size:1.5em;" class="texto-centrado titulo">INFORMACION ADICIONAL AL ASOCIADO</th>
+		</tr>
+		<tr>
+			<th colspan="5" style="padding-top: 40px;">4 x MIL 29,924 SALDO A CARGO 22,014,520 RENDTOS 0</th>
+		</tr>
+	</table>
+	<table style="width: 100%;" cellpadding="3" cellspacing="0">
+		<tr>
+			<th colspan="8" class="texto-centrado titulo" style="font-size: 1.5em;">OTROS AHORROS, REVALORIZACIONES Y DEMAS SALDOS A FAVOR</th>
+		</tr>
+		<tr>
+			<th class="texto-centrado">NUMERO <br>CUENTA</th>
+			<th class="texto-centrado">MODALIDAD DE AHORRO</th>
+			<th class="texto-centrado">FECHA DE <br>EMISION</th>
+			<th class="texto-centrado">FECHA DE <br>VENCTO</th>
+			<th class="texto-centrado">VALOR CUOTA <br>AHORRO</th>
+			<th class="texto-centrado"></th>
+			<th class="texto-centrado">SALDO <br>ACTUAL</th>
+			<th class="texto-centrado">RENDTOS POR <br>PAGAR</th>
+		</tr>
+		<tr>
+			<td>0000002906</td>
+			<td>AHORRO VOLUNTARIO</td>
+			<td>2017.04.30</td>
+			<td>2030.12.31</td>
+			<td class="texto-derecho">0</td>
+			<td></td>
+			<td class="texto-derecho">206,506</td>
+			<td class="texto-derecho">0</td>
+		</tr>
+		<tr>
+			<td>0000013242</td>
+			<td>AHORRO VACACIONES</td>
+			<td>2012.12.15</td>
+			<td>2030.12.31</td>
+			<td class="texto-derecho">0</td>
+			<td>S</td>
+			<td class="texto-derecho">6,212</td>
+			<td class="texto-derecho">0</td>
+		</tr>
+		<tr>
+			<th colspan="6">Total Ahorros a la Vista, CDATS y Rendimientos</th>
+			<td class="texto-derecho">212,718</td>
+			<td class="texto-derecho">0</td>
+		</tr>
+		<tr>
+			<th colspan="8" class="texto-centrado titulo" style="font-size: 1.5em;">CUOTAS EXTRAORDINARIAS</th>
+		</tr>
+	</table>
+	<table style="width: 100%;" cellpadding="2" cellspacing="0">
+		<tr>
+			<th>FECHA DE <br>PAGO</th>
+			<th>VALOR <br>PACTADO</th>
+			<th>NUMERO <br>CREDITO</th>
+			<th>MOD.</th>
+			<th>FECHA DE <br>PAGO</th>
+			<th>VALOR <br>PACTADO</th>
+			<th>NUMERO <br>CREDITO</th>
+			<th>MOD.</th>
+			<th>FECHA DE <br>PAGO</th>
+			<th>VALOR <br>PACTADO</th>
+			<th>NUMERO <br>CREDITO</th>
+			<th>MOD.</th>
+		</tr>
+		<tr>
+			<td>2019.12.30</td>
+			<td>420750</td>
+			<td>5000366596</td>
+			<td>REV</td>
+			<td>2020.02.28</td>
+			<td>1,533,000</td>
+			<td>5000366596</td>
+			<td>REV</td>
+			<td>2020.06.30</td>
+			<td>420,750</td>
+			<td>5000366596</td>
+			<td>REV</td>
+		</tr>
+		<tr>
+			<td>2019.12.30</td>
+			<td>420750</td>
+			<td>5000366596</td>
+			<td>REV</td>
+			<td>2020.02.28</td>
+			<td>1,533,000</td>
+			<td>5000366596</td>
+			<td>REV</td>
+			<td>2020.06.30</td>
+			<td>420,750</td>
+			<td>5000366596</td>
+			<td>REV</td>
+		</tr>
+		<tr>
+			<td>2019.12.30</td>
+			<td>420750</td>
+			<td>5000366596</td>
+			<td>REV</td>
+			<td>2020.02.28</td>
+			<td>1,533,000</td>
+			<td>5000366596</td>
+			<td>REV</td>
+			<td>2020.06.30</td>
+			<td>420,750</td>
+			<td>5000366596</td>
+			<td>REV</td>
+		</tr>
+		<tr>
+			<td>2019.12.30</td>
+			<td>420750</td>
+			<td>5000366596</td>
+			<td>REV</td>
+			<td>2020.02.28</td>
+			<td>1,533,000</td>
+			<td>5000366596</td>
+			<td>REV</td>
+			<td>2020.06.30</td>
+			<td>420,750</td>
+			<td>5000366596</td>
+			<td>REV</td>
+		</tr>
+		<tr>
+			<td>2019.12.30</td>
+			<td>420750</td>
+			<td>5000366596</td>
+			<td>REV</td>
+			<td>2020.02.28</td>
+			<td>1,533,000</td>
+			<td>5000366596</td>
+			<td>REV</td>
+			<td>2020.06.30</td>
+			<td>420,750</td>
+			<td>5000366596</td>
+			<td>REV</td>
+		</tr>
+		<tr>
+			<th colspan="12" class="texto-centrado titulo" style="font-size: 1.5em;">SALDOS DE CARTERA</th>
+		</tr>
+	</table>
+	<table style="width: 100%;" cellpadding="2" cellspacing="0">
+		<tr>
+			<th>NUMERO <br>PTMO</th>
+			<th>LINEA DE CREDITO</th>
+			<th>FECHA DE <br>EMISION</th>
+			<th>FECHA DE <br>VENCTO</th>
+			<th>VALOR <br>INICIAL</th>
+			<th>CTAS <br>PTES</th>
+			<th>VALOR <br>CUOTA</th>
+			<th>SALDO EN <br>MORA</th>
+			<th>SALDO A <br>CAPITAL</th>
+			<th>INT./SEGUR. <br>OTR./CONC.</th>
+			<th>SDO TOTAL <br>DE LA DEUDA</th>
+		</tr>
+		<tr>
+			<td>5000362983</td>
+			<td>COOPSERFUN - LOS OLIVOS</td>
+			<td>2019.07.01</td>
+			<td>2019.07.30</td>
+			<td class="texto-derecho">6,730</td>
+			<td class="texto-derecho">2</td>
+			<td class="texto-derecho">3,365</td>
+			<td class="texto-derecho">3,365</td>
+			<td class="texto-derecho">6,730</td>
+			<td></td>
+			<td class="texto-derecho">6,730</td>
+		</tr>
+		<tr>
+			<td>5000366592</td>
+			<td>CRED.SUMIN.37 A 48 MESES</td>
+			<td>2019.07.22</td>
+			<td>2023.05.30</td>
+			<td class="texto-derecho">5,584,172</td>
+			<td class="texto-derecho">47</td>
+			<td class="texto-derecho">170,000</td>
+			<td class="texto-derecho"></td>
+			<td class="texto-derecho">5,584,172</td>
+			<td class="texto-derecho">27,273</td>
+			<td class="texto-derecho">5,611,445</td>
+		</tr>
+		<tr>
+			<td>5000366596</td>
+			<td>REC EXT VIVIENDA</td>
+			<td>2019.07.22</td>
+			<td>2024.06.30</td>
+			<td class="texto-derecho">23,250,496</td>
+			<td class="texto-derecho">60</td>
+			<td class="texto-derecho">380,000</td>
+			<td class="texto-derecho"></td>
+			<td class="texto-derecho">23,250,496</td>
+			<td class="texto-derecho">96,815</td>
+			<td class="texto-derecho">23,347,311</td>
+		</tr>
+		<tr>
+			<td>5000370749</td>
+			<td>SS ODONTOLOGICOS Y</td>
+			<td>2019.07.31</td>
+			<td>2021.01.30</td>
+			<td class="texto-derecho">500,000</td>
+			<td class="texto-derecho">18</td>
+			<td class="texto-derecho">30,612</td>
+			<td class="texto-derecho"></td>
+			<td class="texto-derecho">500,000</td>
+			<td class="texto-derecho"></td>
+			<td class="texto-derecho">500,000</td>
+		</tr>
+		<tr>
+			<th colspan="4" class="texto-centrado">Totales</th>
+			<td class="texto-derecho">29,341,398</td>
+			<td></td>
+			<td class="texto-derecho">583,977</td>
+			<td class="texto-derecho">3,365</td>
+			<td class="texto-derecho">29,341,398</td>
+			<td class="texto-derecho">124,088</td>
+			<td class="texto-derecho">29,465,486</td>
 		</tr>
 	</table>
 </body>
